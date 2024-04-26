4fd40b5e658df701355fbf9b68351f4f7d82ee72
Anlor <chenxiex@outlook.com>
2024-04-26 11:15:41 +0800

    Calculate the correct value instead of using a fix value in order to be used on different platform.

--- bdprochot_on.sh
+++ bdprochot_on.sh
@@ -2,11 +2,10 @@
 #  
 
 sudo modprobe msr 
-# r=`sudo rdmsr 0x1FC`
-# s='0x'$r'' 
-# f=$(($s|0x00001))
+r=`sudo rdmsr 0x1FC`
+s='0x'$r'' 
+f=$(($s|0x1))
 
-# sudo wrmsr 0x1FC "obase=16;$f"|bc
-sudo wrmsr 0x1FC 4005d
+sudo wrmsr 0x1FC $f
 
 echo "BD PROCHOT on."
